474c5165791ed86cebe87ac2ca0e30a949ab8955
ypreiser <ypreiser@gmail.com>
2024-05-05 16:15:05 +0300

    check

---
+++ src/functions/check.js
@@ -0,0 +1,33 @@
+let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+const winningSets = [
+    "012",
+    "345",
+    "678",
+    "036",
+    "147",
+    "258",
+    "048",
+    "246"
+]
+
+const checkWin = (Number) => {
+    let setsToCheck = []
+    winningSets.forEach(element => {
+        if (element.includes(Number)) {
+            setsToCheck.push(element)
+        }
+    })
+    setsToCheck.forEach(set => {
+        if (board[set[0]] == board[set[1]] && board[set[0]] == board[set[2]]) return true
+    })
+}
+
+
+// const checkWin2 = (Number) => {
+//     if (board[Number] == board[Number + 1] &&(board[Number] == board[Number + 2]) return true
+//     || (board[Number] == board[Number + 3] == board[Number + 6]) return true
+//     || (board[Number] == board[Number + 4] == board[Number + 8]) return true
+//     || (board[Number] == board[Number - 1] == board[Number - 2]) return true
+//      (board[Number] == board[Number - 4] == board[Number - 8]) return true
+// }
