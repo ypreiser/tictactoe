75b44ce3143d51241a5dfd3c60d1d0907c3a1872
ypreiser <ypreiser@gmail.com>
2024-05-12 12:36:20 +0300

    code

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styles from './styles.module.scss'
 
-export default function Input({title}) {
+export default function Input({  code, setCode }) {
+
+
   return (
-    
-    <input type="text" />
+    <input type="number" value={code} onChange={(e)=>{setCode(e.target.value)}} placeholder='Enter game code' />
   )
 }
