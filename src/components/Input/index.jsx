46ade051cb30986607a604a77a77d72ac578f25e
IsraBA <IsraelBenAri1000@gmail.com>
2024-05-12 13:05:58 +0300

    input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,20 @@
 import React, { useRef, useEffect } from 'react'
 import styles from './styles.module.scss'
 
-export default function Input({  code, setCode }) {
+export default function Input({ code, setCode }) {
 
+  useEffect(() => {
+    if (code.length > 6) {
+      setCode(code.slice(0, 6))
+    }
+  }, [code])
 
   return (
-    <input type="number" value={code} onChange={(e)=>{setCode(e.target.value)}} placeholder='Enter game code' />
+    <input
+    className={styles.input}
+    type="number" 
+    value={code} 
+    onChange={(e) => { setCode(e.target.value) }} 
+    placeholder='Enter game code' />
   )
 }
